c3e2fb9189ae1cc6a400f476ce2f79f12b04c944
Jeremy Josol <jeremyjosol@gmail.com>
2024-01-12 15:36:26 -0800

    :hammer_and_wrench:

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,10 +14,11 @@ const Hero = () => {
         <h1><span className='hello-world'>Hello, World!</span>
         <br /> 
         <span className='becoming-gradient'>I'm</span> <span className='gradient'>Jeremy</span> <span className='gradient'>Josol.</span></h1>
-        <p>I'm a web developer based out of Portland, OR. I'm passionate about implementing creative design solutions to enhance and modernize user application experiences.</p>
+        <p>I'm a web developer based out of Portland, OR 
+        & I am passionate about implementing creative design solutions w/ clean modularized code to enhance & modernize user application experiences.</p>
         <div className='button-container'>
-          <button className="contact-button"><IoMdMail className='icons' /> CONTACT</button>
           <button className="about-button"> ABOUT <BsArrowRightCircle className='icons' /></button>
+          <button className="contact-button"><IoMdMail className='icons' /> CONTACT</button>
         </div>
       </div>
     </div>
