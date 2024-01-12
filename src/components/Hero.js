1118c68e92ace88a034783416b78c31cd042a9f4
Jeremy Josol <jeremyjosol@gmail.com>
2024-01-12 14:03:59 -0800

    Add Hero context + work on structure & styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import './Hero.css';
+import headshot from './../img/headshot.jpg';
 
 const Hero = () => {
   return (
     <div className="hero-container">
       <div className="headshot">
-        <img src="" alt="" />
+        <img src={headshot} alt="" />
       </div>
       <div className="hero-details">
-        <h1>Jeremy Josol</h1>
-        <p>Web Developer</p>
-        <p>Lorem ipsum</p>
+        <h1>Hello, World! 
+        <br /> 
+        I'm Jeremy Josol.</h1>
+        <p>I'm a web developer based out of Portland, OR. I'm passionate about implementing creative design solutions to enhance and modernize user application experiences.</p>
+        <button className="contact-button">Contact Me</button>
       </div>
     </div>
   );
