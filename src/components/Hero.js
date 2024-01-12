0e5733f8499ffbd644d67a5c857146a3ffd43a83
Jeremy Josol <jeremyjosol@gmail.com>
2024-01-12 15:26:07 -0800

    Add About button w/ styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Hero.css';
 import headshot from './../img/headshot.jpg';
 import { IoMdMail } from "react-icons/io";
+import { BsArrowRightCircle } from "react-icons/bs";
 
 const Hero = () => {
   return (
@@ -14,7 +15,10 @@ const Hero = () => {
         <br /> 
         <span className='becoming-gradient'>I'm</span> <span className='gradient'>Jeremy</span> <span className='gradient'>Josol.</span></h1>
         <p>I'm a web developer based out of Portland, OR. I'm passionate about implementing creative design solutions to enhance and modernize user application experiences.</p>
-        <button className="contact-button"><IoMdMail className='icons' /> CONTACT</button>
+        <div className='button-container'>
+          <button className="contact-button"><IoMdMail className='icons' /> CONTACT</button>
+          <button className="about-button"> ABOUT <BsArrowRightCircle className='icons' /></button>
+        </div>
       </div>
     </div>
   );
