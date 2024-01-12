613f9ec214eccd29d82372415cac20e4518745e1
Jeremy Josol <jeremyjosol@gmail.com>
2024-01-12 12:16:44 -0800

    Create boilerplate structure w/ Hero component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Hero from './Hero';
+
+function App() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="headshot">
+        <img src="" alt="" />
+      </div>
+      <div className="hero-details">
+        <h1>Jeremy Josol</h1>
+        <p>Web Developer</p>
+        <p>Lorem ipsum</p>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
