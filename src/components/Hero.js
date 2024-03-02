bfd00e2808a0908dfd76d98bc4552e8e2eae6ac1
Jeremy Josol <jeremyjosol@gmail.com>
2024-03-02 12:53:45 -0800

    Toggle sidebar w/ button hover

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,11 +8,6 @@ import Sidebar from './Sidebar';
 const Hero = () => {
   const [showSidebar, setShowSidebar] = useState(false);
 
-  const toggleSidebar = () => {
-    setShowSidebar(!showSidebar);
-    console.log("showSidebar:", showSidebar); 
-  };
-
   return (
     <div className="hero-container">
       <div className="headshot">
@@ -25,15 +20,20 @@ const Hero = () => {
         <p>I'm a web developer based out of Portland, OR 
         & I am passionate about implementing creative design solutions w/ clean modularized code to enhance & modernize user application experiences.</p>
         <div className='button-container'>
-          <button className="toggle-button" onClick={toggleSidebar}>Toggle Sidebar</button>
+          <button 
+            className="toggle-button" 
+            onMouseEnter={() => setShowSidebar(true)} 
+            onMouseLeave={() => setShowSidebar(false)}
+          >
+            Toggle Sidebar
+          </button>
           <button className="about-button"> ABOUT <BsArrowRightCircle className='icons' /></button>
           <button className="contact-button"><IoMdMail className='icons' /> CONTACT</button>
         </div>
       </div>
-      {showSidebar && <Sidebar showSidebar={showSidebar} />}
+      <Sidebar showSidebar={showSidebar} />
     </div>
   );
 }
 
 export default Hero;
-
