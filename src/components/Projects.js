6a60f59338871fca47a25e310afc7d5057041d8f
Jeremy Josol <jeremyjosol@gmail.com>
2024-03-06 17:23:24 -0800

    Work on component structure

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,21 +4,21 @@ import projectOne from './../img/less-but-better.png'
 
 const Projects = () => {
   const projects = [
-    { id: 1, title: '', imageUrl: projectOne },
-    { id: 2, title: '', imageUrl: '' },
-    { id: 3, title: '', imageUrl: '' },
-    { id: 4, title: '', imageUrl: ''},
+    { id: 1, title: 'Less, But Better', imageUrl: projectOne },
+    { id: 2, title: 'Project 2', imageUrl: '' },
+    { id: 3, title: 'Project 3', imageUrl: '' },
+    { id: 4, title: 'Project 4', imageUrl: ''},
   ];
 
   return (
     <div className="projects-container">
-      <h2>Projects</h2>
+      <h2 className='projects-header'>projects</h2>
       <div className="projects-grid">
         {projects.map(project => (
           <div key={project.id} className="project-card">
             <img src={project.imageUrl} alt={project.title} />
-            <div className="overlay"></div> {/* Overlay div */}
-            <h3>{project.title}</h3>
+            <h3 className="project-title">{project.title}</h3>
+            <div className="overlay"></div>
           </div>
         ))}
       </div>
