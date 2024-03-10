a248edea5b9b05f7080e76fa4e91ff86ed6c8c19
Jeremy Josol <jeremyjosol@gmail.com>
2024-03-10 13:58:25 -0700

    Edit context

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
     <div className="about-container">
       <h1>"ABOUT" <BsAsterisk className='asterisk' /></h1>
       <p className="about-content">
-        I grew up in San Francisco, CA where I built a strong foundation of love for art and culture—  elements that I strive to connect into my professional work. As a multidisciplinary artist, I create various mediums including and not limited to music, graphic/web design, approaching each with passion and synergy.
+        Growing up in San Francisco, CA, I built a strong foundation of love for art and culture—  elements that I strive to connect into my professional work. As a multidisciplinary artist, I create various mediums including and not limited to music, graphic/web design, approaching each with passion and synergy.
         <br />
         <br /> 
         The art of coding has allowed me the opportunity to freely translate my concepts into visual forms while challenging my problem-solving capabilities through research and practice of influential design principles.
